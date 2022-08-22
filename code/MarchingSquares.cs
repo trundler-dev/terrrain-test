a8d776e65fbf3492c23b1ac7d3d5164d3d1c973c
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-08-22 08:53:19 -0300

    add wall model and whatnot

--- code/MarchingSquares.cs
+++ code/MarchingSquares.cs
@@ -16,7 +16,11 @@ public class MarchingSquares : ModelEntity
 	public int Width { get; set; }
 	public int Height { get; set; }
 
-	public float surfaceLevel { get; set; } = 0.5f;
+	public float surfaceLevel { get; set; } = 0.55f;
+
+	public Dictionary<int, List<Triangle>> TriangleDictionary = new();
+	public List<List<int>> Outlines = new();
+	public HashSet<int> CheckedVertices = new();
 
 	public override void Spawn()
 	{
@@ -37,15 +41,41 @@ public class MarchingSquares : ModelEntity
 	{
 		TerrainGrid = new bool[Width, Height];
 
+		// int r = Rand.Int( 0, 1000 );
+		int r = 0;
+
 		for ( int x = 0; x < Width; x++ )
 			for ( int y = 0; y < Height; y++ )
 			{
-				TerrainGrid[x, y] = Noise.Simplex( x, y ) > surfaceLevel;
+				TerrainGrid[x, y] = Noise.Simplex( x + r, y + r ) > surfaceLevel;
 			}
+
+		// Add a border to our generated map
+		int borderSize = 5;
+		bool[,] borderedMap = new bool[Width + borderSize * 2, Height + borderSize * 2];
+
+		for ( int x = 0; x < borderedMap.GetLength( 0 ); x++ )
+			for ( int z = 0; z < borderedMap.GetLength( 1 ); z++ )
+			{
+				if ( x >= borderSize && x < Width + borderSize && z >= borderSize && z < Height + borderSize )
+				{
+					borderedMap[x, z] = TerrainGrid[x - borderSize, z - borderSize];
+				}
+				else
+				{
+					borderedMap[x, z] = true;
+				}
+			}
+
+		TerrainGrid = borderedMap;
 	}
 
 	public void Draw()
 	{
+		Outlines.Clear();
+		CheckedVertices.Clear();
+		TriangleDictionary.Clear();
+
 		for ( int x = 0; x < TerrainGrid.GetLength( 0 ) - 1; x++ )
 			for ( int z = 0; z < TerrainGrid.GetLength( 1 ) - 1; z++ )
 			{
@@ -72,13 +102,13 @@ public class MarchingSquares : ModelEntity
 				switch ( marchCase )
 				{
 					case 1:
-						MeshFromPoints( middleBottom, bottomLeft, middleLeft );
+						MeshFromPoints( middleLeft, middleBottom, bottomLeft );
 						break;
 					case 2:
-						MeshFromPoints( middleRight, bottomRight, middleBottom );
+						MeshFromPoints( bottomRight, middleBottom, middleRight );
 						break;
 					case 4:
-						MeshFromPoints( middleTop, topRight, middleRight );
+						MeshFromPoints( topRight, middleRight, middleTop );
 						break;
 					case 8:
 						MeshFromPoints( topLeft, middleTop, middleLeft );
@@ -121,6 +151,10 @@ public class MarchingSquares : ModelEntity
 					// 4 point:
 					case 15:
 						MeshFromPoints( topLeft, topRight, bottomRight, bottomLeft );
+						CheckedVertices.Add( topLeft.VertexIndex );
+						CheckedVertices.Add( topRight.VertexIndex );
+						CheckedVertices.Add( bottomRight.VertexIndex );
+						CheckedVertices.Add( bottomLeft.VertexIndex );
 						break;
 				}
 			}
@@ -128,7 +162,7 @@ public class MarchingSquares : ModelEntity
 		Log.Info( "Made it past cases" );
 
 		// Convert to Vert struct
-		List<Vert> vertList = new List<Vert>();
+		var vertList = new List<Vert>();
 		foreach ( var vert in Vertices )
 		{
 			vertList.Add( new Vert( vert ) );
@@ -139,16 +173,36 @@ public class MarchingSquares : ModelEntity
 
 		var mesh = new Mesh( Material.Load( "materials/dev/dev_measuregeneric01.vmat" ) )
 		{
-			Bounds = new BBox( 0, 5 )
+			Bounds = new BBox( 0, new Vector3( Width * resolution, 0, Height * resolution ) )
 		};
 		mesh.CreateVertexBuffer( vertList.Count, Vert.Layout, vertList );
 		mesh.CreateIndexBuffer( Triangles.Count, Triangles );
 
 		Log.Info( "Created vertex buffer and index buffer" );
 
-		Model = new ModelBuilder().AddMesh( mesh ).Create();
+		var builder = new ModelBuilder();
 
-		Log.Info( "Created and set model" );
+		if ( IsClient )
+		{
+			builder.AddMesh( mesh );
+		}
+		else
+		{
+			builder.AddCollisionMesh( Vertices.ToArray(), Triangles.ToArray() );
+		}
+
+		Model = builder.Create();
+
+		SetupPhysicsFromModel( PhysicsMotionType.Static );
+
+		CalculateMeshOutlines();
+		WallModel wall = new WallModel();
+		wall.Outlines = Outlines;
+		wall.Vertices = Vertices;
+		wall.Triangles = Triangles;
+		wall.Width = Width;
+		wall.Height = Height;
+		wall.CreateWallMesh();
 	}
 
 	private int GetCase( bool a, bool b, bool c, bool d )
@@ -197,6 +251,102 @@ public class MarchingSquares : ModelEntity
 		Triangles.Add( a.VertexIndex );
 		Triangles.Add( b.VertexIndex );
 		Triangles.Add( c.VertexIndex );
+
+		Triangle triangle = new Triangle( a.VertexIndex, b.VertexIndex, c.VertexIndex );
+		AddTriangleToDictionary( triangle.VertexIndexA, triangle );
+		AddTriangleToDictionary( triangle.VertexIndexB, triangle );
+		AddTriangleToDictionary( triangle.VertexIndexC, triangle );
+	}
+
+	private void AddTriangleToDictionary( int vertexIndexKey, Triangle triangle )
+	{
+		if ( TriangleDictionary.ContainsKey( vertexIndexKey ) )
+		{
+			TriangleDictionary[vertexIndexKey].Add( triangle );
+		}
+		else
+		{
+			List<Triangle> triangleList = new();
+			triangleList.Add( triangle );
+			TriangleDictionary.Add( vertexIndexKey, triangleList );
+		}
+	}
+
+	private bool IsOutlineEdge( int vertexA, int vertexB )
+	{
+		List<Triangle> trianglesContainingVertexA = TriangleDictionary[vertexA];
+		int sharedTriangleCount = 0;
+
+		for ( int i = 0; i < trianglesContainingVertexA.Count; i++ )
+		{
+			if ( trianglesContainingVertexA[i].Contains( vertexB ) )
+			{
+				sharedTriangleCount++;
+				if ( sharedTriangleCount > 1 )
+				{
+					break;
+				}
+			}
+		}
+
+		return sharedTriangleCount == 1;
+	}
+
+	private int GetConnectedOutlineVertex( int vertexIndex )
+	{
+		List<Triangle> trianglesContainingVertex = TriangleDictionary[vertexIndex];
+		for ( int i = 0; i < trianglesContainingVertex.Count; i++ )
+		{
+			Triangle triangle = trianglesContainingVertex[i];
+
+			for ( int j = 0; j < 3; j++ )
+			{
+				int vertexB = triangle[j];
+
+				if ( vertexB != vertexIndex && !CheckedVertices.Contains( vertexB ) )
+				{
+					if ( IsOutlineEdge( vertexIndex, vertexB ) )
+					{
+						return vertexB;
+					}
+				}
+			}
+		}
+
+		return -1;
+	}
+
+	private void CalculateMeshOutlines()
+	{
+		for ( int vertexIndex = 0; vertexIndex < Vertices.Count; vertexIndex++ )
+		{
+			if ( !CheckedVertices.Contains( vertexIndex ) )
+			{
+				int newOutlineVertex = GetConnectedOutlineVertex( vertexIndex );
+				if ( newOutlineVertex != -1 )
+				{
+					CheckedVertices.Add( vertexIndex );
+
+					List<int> newOutline = new();
+					newOutline.Add( vertexIndex );
+					Outlines.Add( newOutline );
+					FollowOutline( newOutlineVertex, Outlines.Count - 1 );
+					Outlines[Outlines.Count - 1].Add( vertexIndex );
+				}
+			}
+		}
+	}
+
+	private void FollowOutline( int vertexIndex, int outlineIndex )
+	{
+		Outlines[outlineIndex].Add( vertexIndex );
+		CheckedVertices.Add( vertexIndex );
+
+		int nextVertexIndex = GetConnectedOutlineVertex( vertexIndex );
+		if ( nextVertexIndex != -1 )
+		{
+			FollowOutline( nextVertexIndex, outlineIndex );
+		}
 	}
 
 	private void DrawLine( Node a, Node b )
@@ -212,6 +362,40 @@ public class MarchingSquares : ModelEntity
 		public Node( Vector3 position )
 		{
 			Position = position;
+		}
+	}
+
+	public struct Triangle
+	{
+		public int VertexIndexA;
+		public int VertexIndexB;
+		public int VertexIndexC;
+
+		public int[] Vertices;
+
+		public Triangle( int a, int b, int c )
+		{
+			VertexIndexA = a;
+			VertexIndexB = b;
+			VertexIndexC = c;
+
+			Vertices = new int[3];
+			Vertices[0] = a;
+			Vertices[1] = b;
+			Vertices[2] = c;
+		}
+
+		public bool Contains( int vertexIndex )
+		{
+			return vertexIndex == VertexIndexA || vertexIndex == VertexIndexB || vertexIndex == VertexIndexC;
+		}
+
+		public int this[int i]
+		{
+			get
+			{
+				return Vertices[i];
+			}
 		}
 	}
 }
---
+++ code/WallModel.cs
@@ -0,0 +1,71 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+
+namespace MyGame;
+
+public class WallModel : ModelEntity
+{
+	public List<List<int>> Outlines = new();
+	public List<Vector3> Vertices { get; set; } = new();
+	public List<int> Triangles { get; set; } = new();
+
+	public int Width { get; set; }
+	public int Height { get; set; }
+
+	private readonly float resolution = 5f;
+
+	public void CreateWallMesh()
+	{
+		List<Vector3> wallVertices = new();
+		List<int> wallTriangles = new();
+
+		float wallHeight = 50f;
+
+		Mesh wallMesh = new Mesh( Material.Load( "materials/dev/dev_measuregeneric01.vmat" ) )
+		{
+			Bounds = new BBox( 0, new Vector3( Width * resolution, wallHeight, Height * resolution ) )
+		};
+
+		foreach ( List<int> outline in Outlines )
+		{
+			for ( int i = 0; i < outline.Count - 1; i++ )
+			{
+				int startIndex = wallVertices.Count;
+				wallVertices.Add( Vertices[outline[i]] );
+				wallVertices.Add( Vertices[outline[i + 1]] );
+				wallVertices.Add( Vertices[outline[i]] - Vector3.Right * wallHeight );
+				wallVertices.Add( Vertices[outline[i + 1]] - Vector3.Right * wallHeight );
+
+				wallTriangles.Add( startIndex + 0 );
+				wallTriangles.Add( startIndex + 2 );
+				wallTriangles.Add( startIndex + 3 );
+
+				wallTriangles.Add( startIndex + 3 );
+				wallTriangles.Add( startIndex + 1 );
+				wallTriangles.Add( startIndex + 0 );
+			}
+		}
+
+		var vertList = new List<Vert>();
+		foreach ( var vert in wallVertices )
+		{
+			vertList.Add( new Vert( vert ) );
+		}
+
+		wallMesh.CreateVertexBuffer( vertList.Count, Vert.Layout, vertList );
+		wallMesh.CreateIndexBuffer( wallTriangles.Count, wallTriangles );
+
+		var builder = new ModelBuilder();
+
+		if ( IsClient )
+		{
+			builder.AddMesh( wallMesh );
+		}
+		else
+		{
+			builder.AddCollisionMesh( wallVertices.ToArray(), wallTriangles.ToArray() );
+		}
+
+		Model = builder.Create();
+	}
+}
